81db77ed2aefedc4e0e636a524b952c7a84de16e
Watсher <Wather@DESKTOP-VL24RQ1>
2022-09-27 00:34:17 +0300

    All methods implimented, all tests are positive.

--- NumeralSystems/Converter.cs
+++ NumeralSystems/Converter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace NumeralSystems
 {
@@ -10,7 +11,10 @@ namespace NumeralSystems
         /// <param name="number">Source number.</param>
         /// <returns>The equivalent string representation of the number in the octal numeral systems.</returns>
         /// <exception cref="ArgumentException">Thrown if number is less than zero.</exception>
-        public static string GetPositiveOctal(this int number) => throw new NotImplementedException(); 
+        public static string GetPositiveOctal(this int number)
+        {
+            return GetPositiveRadix(number, 8);
+        }
 
         /// <summary>
         /// Gets the value of a positive integer to its equivalent string representation in the decimal numeral systems.
@@ -18,7 +22,10 @@ namespace NumeralSystems
         /// <param name="number">Source number.</param>
         /// <returns>The equivalent string representation of the number in the decimal numeral systems.</returns>
         /// <exception cref="ArgumentException">Thrown if number is less than zero.</exception>
-        public static string GetPositiveDecimal(this int number) => throw new NotImplementedException();
+        public static string GetPositiveDecimal(this int number)
+        {
+            return GetPositiveRadix(number, 10);
+        }
 
         /// <summary>
         /// Gets the value of a positive integer to its equivalent string representation in the hexadecimal numeral systems.
@@ -26,8 +33,10 @@ namespace NumeralSystems
         /// <param name="number">Source number.</param>
         /// <returns>The equivalent string representation of the number in the hexadecimal numeral systems.</returns>
         /// <exception cref="ArgumentException">Thrown if number is less than zero.</exception>
-        public static string GetPositiveHex(this int number) => throw new NotImplementedException();
-
+        public static string GetPositiveHex(this int number)
+        {
+            return GetPositiveRadix(number, 16);
+        }
         /// <summary>
         /// Gets the value of a positive integer to its equivalent string representation in a specified radix.
         /// </summary>
@@ -36,7 +45,15 @@ namespace NumeralSystems
         /// <returns>The equivalent string representation of the number in a specified radix.</returns>
         /// <exception cref="ArgumentException">Thrown if radix is not equal 8, 10 or 16.</exception>
         /// <exception cref="ArgumentException">Thrown if number is less than zero.</exception>
-        public static string GetPositiveRadix(this int number, int radix) => throw new NotImplementedException();
+        public static string GetPositiveRadix(this int number, int radix)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("can not be less than zero.");
+            }
+
+            return GetRadix(number, radix);
+        }
         
         /// <summary>
         /// Gets the value of a signed integer to its equivalent string representation in a specified radix.
@@ -45,6 +62,27 @@ namespace NumeralSystems
         /// <param name="radix">Base of the numeral systems.</param>
         /// <returns>The equivalent string representation of the number in a specified radix.</returns>
         /// <exception cref="ArgumentException">Thrown if radix is not equal 8, 10 or 16.</exception>
-        public static string GetRadix(this int number, int radix) => throw new NotImplementedException();
+        public static string GetRadix(this int number, int radix)
+        {
+            if (radix != 8 & radix != 10 & radix != 16)
+            {
+                throw new ArgumentException("is 8, 10 and 16 only.");
+            }
+
+            uint numberCopy;
+            numberCopy = (uint)number;
+            string alphabet = "0123456789ABCDEF";
+            StringBuilder radixNum = new StringBuilder();
+            byte rem = 0;
+            while (numberCopy > 0)
+            {
+                rem = (byte)(numberCopy % radix);
+
+                radixNum.Insert(0, alphabet[rem]);
+                numberCopy /= (uint)radix;
+            }
+
+            return radixNum.ToString();
+        }
     }
 }
